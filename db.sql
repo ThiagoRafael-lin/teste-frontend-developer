26941816600f553f6a5c28a93b5b601435100540
ThiagoRafael-lin <thiagorafael2005@gmail.com>
2026-04-13 14:50:03 -0300

    Estrutura inicial criada

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    mensagem TEXT NOT NULL,
+    data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
