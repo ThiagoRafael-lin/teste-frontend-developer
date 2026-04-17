a2e5f3a04c608ee343766d8cd3c2010ce33381c9
ThiagoRafael-lin <thiagorafael2005@gmail.com>
2026-04-17 16:18:02 -0300

    Deploy feito e ajustes de conexão com banco de dados

--- enviar.php
+++ enviar.php
@@ -1,5 +1,16 @@
 <?php
-require_once 'inc/configuracao.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . '/inc/configuracao.php';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $name     = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
